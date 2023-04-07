20620a3d0af4062f5a575feaefd73498c26aa9b3
Saranesh <saranesh2002@gmail.com>
2023-04-07 14:06:22 +0530

    party type patch

--- library_management/patches/party_type.py
+++ library_management/patches/party_type.py
@@ -4,7 +4,7 @@ def execute():
     frappe.reload_doc("Setup", "doctype", "Party Type")
     
     if not frappe.db.exists("Party Type", "Library Member"):
-        doc = frappe.new_doc("Part Type")
+        doc = frappe.new_doc("Party Type")
         doc.party_type = "Library Member"
         doc.account_type = "Receivable"
         doc.save()
